49e476afe74e1e7e45b0a1eaebba4652c0ee79eb
JessyLee2208 <77054287+JessyLee2208@users.noreply.github.com>
2021-03-12 00:25:04 +0800

    Add files via upload

--- weekThree/app.js
+++ weekThree/app.js
@@ -1,8 +1,13 @@
 //匯入express模組
 var express= require('express');
+// const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 //建立伺服器
 var app=express();
+
+app.set('view engine','pug');
+app.use(bodyParser.urlencoded({extended:false}));
 
 //伺服器位置的名稱 最後網址為http://localhost:3000/
 const hostName = 'localhost';
@@ -38,9 +43,7 @@ app.get('/getData', function (req, res){
     res.send(`${result}`)
 });
 
-app.get ('/myName',function(req, res){
-    res.send('<h1>hihihih</h1>')
-});
+
 
 
 app.use('/public', express.static('public'));
@@ -48,6 +51,21 @@ app.use('/public', express.static('public'));
 app.get ('/sum.html',function(req, res){
     res.sendFile( __dirname + "/" + "sum.html" );
 });
+
+app.get('/trackName',function(req, res){
+    res.render('app');
+
+});
+
+app.post('/trackName', (req, res)=> {
+    res.render('app');
+    console.dir(req.body);
+    // res.cookie('username', req.body.username)
+    
+    
+});
+
+// app.use('cookie',routerCookie);
 
 //監聽3000埠
 const server= app.listen(port, function() {
